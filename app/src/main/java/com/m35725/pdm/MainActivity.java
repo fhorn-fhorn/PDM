e51770f90a42ebe98a0a48c4d8f4ff349fd5c054
fhorn-fhorn <fhorn@bewnet.com.br>
2019-08-13 20:37:05 -0300

    Aula 02

---
+++ app/src/main/java/com/m35725/pdm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.m35725.pdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void aula2Click(View view) {
+
+    }
+}
